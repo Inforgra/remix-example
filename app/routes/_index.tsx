e249bcb74e2d109d115b0d30c107f149e012c18f
Kukjin Kang <kj.kang@daum.net>
2024-09-01 22:51:59 +0900

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useEffect, useRef, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,40 +10,46 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container mx-auto mt-24">
+      <div className="flex">
+        <Dropdown />
+      </div>
+    </div>
+  );
+}
+
+function Dropdown() {
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [dropdownRef]);
+
+  return (
+    <div ref={dropdownRef} className="relative">
+      <button
+        className="p-2 rounded bg-blue-200 hover:bg-blue-100"
+        onClick={ () => setIsOpen(!isOpen) }
+      >
+        menu
+      </button>
+      { isOpen &&
+        <div className="absolute left-0 mt-2 rounded shadow-md min-w-64 bg-gray-200">
+          <a href="https://naver.com" target="_blank" className="flex p-2 hover:bg-gray-100" onClick={ () => setIsOpen(false) }>naver</a>
+          <a href="https://daum.net" target="_blank" className="flex p-2 hover:bg-gray-100" onClick={ () => setIsOpen(false) }>daum</a>
+          <a href="https://google.com" target="_blank" className="flex p-2 hover:bg-gray-100" onClick={ () => setIsOpen(false) }>google</a>
+        </div>
+      }
     </div>
   );
 }
